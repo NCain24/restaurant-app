c34f61536a07dc1f5a6fada58bdcbe31e13eda72
Noah Cain <noahcain.work@gmail.com>
2023-11-14 19:15:01 -0800

    Work on restaurant card

--- src/app/search/RestaurantList.jsx
+++ src/app/search/RestaurantList.jsx
@@ -1,32 +1,41 @@
-async function getRestaurants() {
-  const res = await fetch('http://localhost:4000/restaurants');
-  return res.json();
-}
+import Image from 'next/image';
 
-const RestaurantList = async () => {
-  const restaurants = await getRestaurants();
-
+const RestaurantList = ({ restaurants }) => {
   return (
-    <>
+    <div className="restaurant">
       {restaurants.map((restaurant) => (
         <div
           key={restaurant.id}
-          className="grid grid-cols-3 w-[20%] border rounded-lg gap-4 px-4 py-6"
+          className="flex bg-zinc-300 rounded-lg max-h-[322px] h-[322px] max-w-[345px]"
         >
-          {console.log(restaurant)}
-          <h3 className="text-2xl">{restaurant.name}</h3>
-          <p className="w-[40%]">{restaurant.description}</p>
-          <ul>
-            {restaurant?.tags?.map((tag) => (
-              <li key={tag}>{tag}</li>
-            ))}
-          </ul>
+          <div className="flex flex-col rounded-lg">
+            <Image
+              alt=""
+              className="w-full h-[140px] rounded-lg border-black"
+              src=""
+              width={345}
+              height={140}
+            />
+            <div className="flex flex-col p-4">
+              <div className="mb-2 text-black text-2xl font-normal font-['Arial'] capitalize">
+                {restaurant.name}
+              </div>
+              <div className=" h-8 text-black text-opacity-60 text-sm font-normal text font-['Arial'] capitalize leading-tight">
+                {restaurant.description}
+              </div>
+            </div>
+            <div className="flex items-end pl-4 h-14">
+              <div className=" text-blue-700 text-base font-normal font-['Arial'] uppercase">
+                Learn More
+              </div>
+            </div>
+          </div>
         </div>
       ))}
       {restaurants.length === 0 && (
         <p>Begin searching for a restaurant to populate the list.</p>
       )}
-    </>
+    </div>
   );
 };
 
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import SearchBar from '../components/SearchBar';
-import RestaurantList from './RestaurantList';
+'use client';
 
-const page = () => {
+import React, { useState } from 'react';
+import RestaurantList from './RestaurantList';
+import SearchBar from '../components/SearchBar';
+
+const SearchPage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  const handleSearchInput = (term: any) => {
+    setSearchTerm(term);
+    // Perform your search logic here, e.g., fetch data from an API
+  };
+
+  const handleSearchSubmit = async () => {
+    // Fetch restaurants and filter by tag based on searchTerm.
+    const response = await fetch('http://127.0.0.1:4000/restaurants');
+
+    if (response.ok) {
+      // Filter restaurants and setFilteredRestaurants.
+      const responseJson = await response.json();
+      console.log(responseJson);
+
+      const restaurants = responseJson.filter(
+        (restaurant: { tags: string[] }) =>
+          restaurant?.tags.includes(searchTerm.toLowerCase())
+      );
+
+      console.log(restaurants);
+      setFilteredRestaurants(restaurants);
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-4 pt-10 items-center justify-center">
-      <SearchBar />
-      <RestaurantList />
+    <div>
+      <SearchBar
+        searchTerm={searchTerm}
+        handleSearchInput={handleSearchInput}
+        handleSearchSubmit={handleSearchSubmit}
+      />
+      {/* Add your search results or further UI here */}
+      <RestaurantList restaurants={filteredRestaurants} />
     </div>
   );
 };
 
-export default page;
+export default SearchPage;
