b319e2d94aafe9feee3c6afa14f259aaa3515e50
Noah Cain <noahcain.work@gmail.com>
2023-11-21 21:19:10 -0800

    add auth

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
+import NavMenu from './NavMenu';
 
 const Header = () => {
   return (
-    <div className="h-[5vh]">
+    <div className="flex h-[5%] justify-between">
       <Link className="font-['Josefin_Slab'] font-extrabold text-3xl" href="/">
         Restaurant App
       </Link>
+      <div className='flex justify-center items-center pr-6'>
+        <NavMenu />
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
 import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
-const HomePage = () => {
+const HomePage = async () => {
+  const session = await getServerSession();
   return (
     <div className="flex items-center justify-center flex-col h-[95vh]">
       <h2 className="font-['Arial'] uppercase text-xl">
         Find your next meal here
       </h2>
-      <Link
+      {!session && (
+        <Link
+          className="border-2 rounded-full py-2 px-6 border-red-200"
+          href="/api/auth/signin"
+        >
+          Login
+        </Link>
+      )}
+      {session && (
+        <Link
         className="border-2 rounded-full py-2 px-6 border-red-200"
         href="/search"
       >
         Search
       </Link>
+      )}
+      
     </div>
   );
 };
